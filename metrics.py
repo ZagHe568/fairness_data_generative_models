336f7db4f91fe4007dc2828feaee6f256fe95fa8
hezihao <hezh@vip.qq.com>
2020-02-19 20:00:43 -0800

    defined interfaces for info-gain and entropy

---
+++ metrics.py
@@ -0,0 +1,18 @@
+def info_gain(data, protected_feature_cols):
+    """
+    Information gain. The data can be split into two groups by protected_feature_col.
+    :param data: n*num_of_features. numpy.ndarray
+    :param protected_feature_cols: the col(s) of the protected feature(s). list
+    :return: the infomation gain
+    """
+    return 0
+
+
+def entropy(data, protected_feature_cols):
+    """
+    Entropy. The data can be split into two groups by protected_feature_col.
+    :param data: n*num_of_features. numpy.ndarray
+    :param protected_feature_cols: the col(s) of the protected feature(s). list
+    :return: the entropy
+    """
+    return 0
