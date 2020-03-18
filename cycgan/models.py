c785e5342e9ceebedefb297eca6fb073e5f3e1e2
HEZIHAO <zihaohe@HEZIHAOs-MacBook-Pro.local>
2020-03-17 23:39:10 -0700

    initial cycgan

---
+++ cycgan/models.py
@@ -0,0 +1,52 @@
+import torch.nn as nn
+
+
+class Generater(nn.Module):
+    def __init__(self, n_features):
+        super(Generater, self).__init__()
+        model = []
+
+        # downsampling layers
+        model += [nn.Linear(n_features, 32),
+                  nn.ReLU(inplace=True),
+                  nn.Linear(32, 16),
+                  nn.ReLU(inplace=True),
+                  nn.Linear(16, 4),
+                  nn.ReLU(inplace=True)]
+
+        # upsampling layers
+        model += [nn.Linear(4, 16),
+                  nn.ReLU(inplace=True),
+                  nn.Linear(16, 32),
+                  nn.ReLU(inplace=True),
+                  nn.Linear(32, n_features),
+                  nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, n_features):
+        super(Discriminator, self).__init__()
+        model = []
+
+        # downsampling layers
+        model += [nn.Linear(n_features, 32),
+                  nn.ReLU(inplace=True),
+                  nn.Linear(32, 16),
+                  nn.ReLU(inplace=True),
+                  nn.Linear(16, 4),
+                  nn.ReLU(inplace=True)]
+
+        # FCN classification layer
+        model += [nn.Linear(4, 1),
+                  nn.Sigmoid()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
