6c541dbc5a5b1a8dade0c9c2497f3b12e787647e
negar-mokhberian <negar.mokhberian@gmail.com>
2020-03-25 16:43:15 -0700

    prev io mapping num of columns and order is fixed

--- io_mapping.py
+++ io_mapping.py
@@ -7,14 +7,22 @@ class InOutMapping:
 
     def __init__(self):
         self.binary_vars = {}
-        self.expand_colnames = []
+        self.onehot_colnames = []
         self.mean = []
         self.sd = []
         self.num_cols_idx = []
         self.num_cols = []
+        self.ordered_columns = []
 
     def map_input(self, features, dummies):
-        expand_features = pd.get_dummies(features, columns=dummies)
+        self.ordered_columns = features.columns
+        for idx, c in enumerate(features.columns):
+            if c not in dummies:
+                #self.num_cols_idx.append(idx)
+                self.num_cols.append(c)
+
+        expand_features = pd.get_dummies(features[dummies], columns=dummies)
+        # reduce the binary variables to only one column
         for c in dummies:
             if features[c].nunique() == 2:
                 self.binary_vars[c] = features[c].unique()
@@ -23,43 +31,52 @@ class InOutMapping:
                 expand_features = expand_features.drop(columns=[new_col])
                 # features = features.drop(columns=['sex_Male', 'Y_<=50K'])
 
-        self.expand_colnames = expand_features.columns
-        for idx, c in enumerate(self.expand_colnames):
-            if not any(map(c.startswith, dummies)):
-                self.num_cols_idx.append(idx)
-                self.num_cols.append(c)
+
+        self.onehot_colnames = expand_features.columns
+
 
         scaler = sklearn.preprocessing.StandardScaler()
-        X_scaled = scaler.fit_transform(expand_features)
+        print(expand_features.shape)
+        print(features[self.num_cols].shape)
+        X = np.column_stack((features[self.num_cols].values, expand_features.values))
+        print(X.shape)
+        X_scaled = scaler.fit_transform(X)
         self.mean = scaler.mean_
         self.sd = np.sqrt(scaler.var_)
-        X = X_scaled.astype(np.float32)
-        #print(X.shape)
-        return X
+        print(X_scaled.shape)
+        X_scaled = X_scaled.astype(np.float32)
+        return X_scaled
 
     def map_output(self, X, dummies, threshold=1): # TODO have the threshold
-        revert_df = pd.DataFrame(columns=dummies)
+        dummies_df = pd.DataFrame(columns=dummies)
         for c in dummies:
-            dummy_idx = [idx for idx, val in enumerate(self.expand_colnames) if val.startswith(c + '_')]
+            dummy_idx = [len(self.num_cols)+idx for idx, val in enumerate(self.onehot_colnames) if val.startswith(c + '_')]
             corresponding = X[:, dummy_idx]
             if c in self.binary_vars.keys():
                 b0 = self.binary_vars[c][0]
                 b1 = self.binary_vars[c][1]
-                values = [b1 if val < 0.5 else b0 for val in corresponding]  # TODO we don't have 0 and 1
+                values = [b1 if val < 0.5 else b0 for val in corresponding]
             else:
                 m = np.zeros_like(corresponding)
                 m[np.arange(len(corresponding)), corresponding.argmax(1)] = 1
                 max_col = np.argmax(m, axis=1)
-                values = [self.expand_colnames[dummy_idx[i]].replace(c + '_', '') for i in max_col]
-            revert_df[c] = values  # TODO eliminate revert_df use res in
+                values = [self.onehot_colnames[dummy_idx[i]-len(self.num_cols)].replace(c + '_', '') for i in max_col]
+            dummies_df[c] = values  # TODO eliminate dummies_df use res in
 
-        res = pd.DataFrame(X[:, self.num_cols_idx], columns=self.num_cols)
+        res = pd.DataFrame(X[:, 0:len(self.num_cols)], columns=self.num_cols)
         for c in dummies:
-            res[c] = revert_df[c]
-        for i in self.num_cols_idx:
-            c = self.expand_colnames[i]
-            res[c] = res[c] * self.sd[i] + self.mean[i]
-        return res
+            res[c] = dummies_df[c]
+        # scale back
+        for i in range(len(self.num_cols)):
+            c = self.num_cols[i]
+            res[c] = int(res[c] * self.sd[i] + self.mean[i])
+        # print(res.shape)
+        # print(res.columns)
+        reorder_res = pd.DataFrame()
+        for c in self.ordered_columns:
+            reorder_res[c] = res[c]
+        print(reorder_res.shape)
+        return reorder_res
 
 
 class InOutMapping2:
